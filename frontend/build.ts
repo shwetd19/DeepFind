35916d3a3a9bd07d5beb1dd414025340f91bec5e
Shwetas Dhake <shwetasdhake16@gmail.com>
2026-07-15 18:20:36 +0530

    frontend build addition

--- frontend/build.ts
+++ frontend/build.ts
@@ -14,6 +14,7 @@ const result = await Bun.build({
   minify: true,
   target: "browser",
   sourcemap: "linked",
+  env: "BUN_PUBLIC_*",
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
